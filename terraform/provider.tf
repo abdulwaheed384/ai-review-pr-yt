7746317fcff28ad2390f15a94b8b552a8938849a
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-06-09 15:54:17 +0100

    video04

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,25 @@
+terraform {
+
+  cloud {
+
+    organization = "ai-tf-yt-org"
+
+    workspaces {
+      name = "ai-pr-review-dev"
+    }
+
+  }
+
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+
+  }
+}
+
 provider "azurerm" {
   features {}
 }
