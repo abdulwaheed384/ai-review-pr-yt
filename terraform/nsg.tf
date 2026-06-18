d6a971009d225835e03acc36920d79b83d3b1435
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-06-18 14:28:42 +0100

    video05-06

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -4,6 +4,11 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_network_security_rule" "allow_ssh" {
   name                        = "Allow-SSH"
   priority                    = 100
