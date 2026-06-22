7cfe1494e2a1342c5604c0851e32492f0fb92e23
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-06-22 12:27:36 +0100

    tfsec and checkov implemented-004

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  dns_servers = ["10.0.0.10", "10.0.0.20"]
+  dns_servers = ["4.2.2.2", "8.8.8.8"]
 }
 
 resource "azurerm_subnet" "subnet" {
