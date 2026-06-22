b1fe16f5a8f29f0473f6ce5b64948e123d10d56d
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-06-22 12:22:22 +0100

    tfsec and checkov implemented-002

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -46,7 +46,7 @@ resource "azurerm_network_security_rule" "allow_rdp" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "3389"
-  source_address_prefix       = "0.0.0.0/0"
+  source_address_prefix       = "192.168.120.0/24"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
