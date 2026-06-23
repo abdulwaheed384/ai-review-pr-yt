58e3174849c7ef4f06a8af84a244f1c58d186ae7
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-06-23 15:34:03 +0100

    AI Agent-101

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -39,7 +39,7 @@ resource "azurerm_network_security_rule" "allow_http" {
 
 
 resource "azurerm_network_security_rule" "allow_rdp" {
-  name                        = "Allow-RDP6"
+  name                        = "Allow-RDP7"
   priority                    = 300
   direction                   = "Inbound"
   access                      = "Allow"
