a9a04fc70a03f0cd70c24ec35e9b7608afff421d
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-07-07 14:37:32 +0100

    video06-102

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "blob" {
-  name                     = "aireviewblobdemo01"
+  name                     = "blobfatherai0707"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
