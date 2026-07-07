b729e41a77f4e1c1959ecb1b9608355730164422
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-07-07 14:46:47 +0100

    video06-104

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -7,12 +7,12 @@ resource "azurerm_storage_account" "blob" {
   account_kind             = "StorageV2"
 
   min_tls_version                 = "TLS1_2"
-  allow_nested_items_to_be_public = true
-  public_network_access_enabled   = true
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled   = false
 }
 
 resource "azurerm_storage_container" "blob" {
   name                  = "documents"
   storage_account_id    = azurerm_storage_account.blob.id
-  container_access_type = "container"
+  container_access_type = "private"
 }
